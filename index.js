7bcbaf8784b41e8c328e371a25c499500f8fd4ff
uconnwomensbball <119827576+uconnwomensbball@users.noreply.github.com>
2024-12-19 21:04:51 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ function calculateMortgage(principal, annualInterestRate, years){
  
     if (radioBtnRepayment.checked){
         radioBtnRepayment.classList.add("lime-color-background")
-        console.log("radiobtn repayment checked!")
         containerRight.innerHTML = `
         <p class = "your-results-p white-text"><strong>Your results</strong></p>
         <p class="your-results-p-text small-text slate-three-hundred-text">Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again.</p>
@@ -67,12 +66,13 @@ function calculateMortgage(principal, annualInterestRate, years){
     }
     else{
         containerRight.innerHTML = `
-        <h3><strong>Your results</strong></h3>
-        <p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again.</p>
+        <p class = "your-results-p white-text"><strong>Your results</strong></p>
+        <p class="your-results-p-text small-text slate-three-hundred-text">Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again.</p>
         <div class ="results-container">
-            <p>Your monthly payments in interest only</p>
+            <p class="your-monthly-payments-text small-text">Your monthly payments in interest only</p>
             <h1 id = "monthly-repayment-result-text" class = "lime-color-text">$${monthlyInterest.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h1>
-            <p>Total interest you'll repay over the ${years}-year term</p>
+            <hr>
+            <p class="small-text">Total interest you'll repay over the ${years}-year term</p>
             <p class = "white-text"><strong>$${totalInterest.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</strong></p>
         </div>
         `
@@ -81,78 +81,80 @@ function calculateMortgage(principal, annualInterestRate, years){
 
 //MORTGAGE AMOUNT FOCUS-BLUR
 mortgageAmountInput.addEventListener('focus', () => {
-        mortgageAmountInput.style.border = "2px solid hsl(61, 70%, 52%)";
-        iconAmount.style.backgroundColor = "hsl(61, 70%, 52%)"})
+    iconAmount.style.backgroundColor = "hsl(61, 70%, 52%)"; 
+    iconAmount.style.color = "hsl(202, 55%, 16%)"
+    mortgageAmountWarning.style.display = "none"})    
         
 mortgageAmountInput.addEventListener('blur', () => {
-    mortgageAmountInput.style.border = "";
     iconAmount.style.backgroundColor = ""})
 
 //MORTGAGE TERM FOCUS-BLUR
 mortgageTermInput.addEventListener("focus", ()=>{
-    mortgageTermInput.style.border = "2px solid hsl(61, 70%, 52%)"
-    iconMortgageTerm.style.backgroundColor = "hsl(61, 70%, 52%)"
-})
+    iconMortgageTerm.style.backgroundColor = "hsl(61, 70%, 52%)";
+    iconMortgageTerm.style.color = "hsl(202, 55%, 16%)"
+    mortgageTermWarning.style.display = "none"})
+    
 mortgageTermInput.addEventListener("blur", ()=>{
-    mortgageTermInput.style.border = ""; 
-    iconMortgageTerm.style.backgroundColor = ""
-})
+    iconMortgageTerm.style.backgroundColor = ""})
 
 //INTEREST RATE FOCUS-BLUR
 interestRateInput.addEventListener("focus", ()=>{
-    interestRateInput.style.border = "2px solid hsl(61, 70%, 52%)"
     iconInterestRate.style.backgroundColor = "hsl(61, 70%, 52%)"
-})
+    iconInterestRate.style.color = "hsl(202, 55%, 16%)"
+    interestRateWarning.style.display = "none"})
+    
 interestRateInput.addEventListener("blur", ()=>{
-    interestRateInput.style.border = ""; 
-    iconInterestRate.style.backgroundColor = ""
-})
+    iconInterestRate.style.backgroundColor = ""})
 
 //RADIO-BTNS CHANGE
 radioBtnRepayment.addEventListener("change", function () {
     if (radioBtnRepayment.checked) {
         repaymentDiv.classList.add("lime-color-background");
-        interestDiv.classList.remove("lime-color-background"); 
-    }
-});
+        interestDiv.classList.remove("lime-color-background")}
+})
 
 radioBtnInterest.addEventListener("change", function () {
     if (radioBtnInterest.checked) {
         interestDiv.classList.add("lime-color-background");
-        repaymentDiv.classList.remove("lime-color-background"); 
-    }
-});
+        repaymentDiv.classList.remove("lime-color-background")}
+})
 
 function emptyScreenRight(){
     containerRight.innerHTML = `
     <img src="./images/illustration-empty.svg"/>
     <h4>Results shown here</h4>
-    <p class = "slate-three-hundred-text small-text">Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>`
+    <p class = "results-shown-here-text slate-three-hundred-text small-text">Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>`
 }
 
 function checkInputs(){
+    if (mortgageAmountInput.value < 1 || mortgageAmountInput.value > 999999){
+        mortgageAmountInput.value = ""}
+    
+    if (mortgageTermInput.value < 1 || mortgageTermInput.value > 99){
+        mortgageTermInput.value = ""}
+    
+    if (interestRateInput.value < 1 || interestRateInput.value > 100){
+        interestRateInput.value = ""}
+    
     if (mortgageAmountInput.value === ""){
         mortgageAmountWarning.style.display = "flex"
-        
-    }
+        iconAmount.style.backgroundColor = "red"
+        iconAmount.style.color = "white"}
     else {
-        mortgageAmountWarning.style.display = "none"
-    }
+        mortgageAmountWarning.style.display = "none"}
     
     if (mortgageTermInput.value === ""){
         mortgageTermWarning.style.display = "flex"
-    }
+        iconMortgageTerm.style.backgroundColor = "red"
+        iconMortgageTerm.style.color = "white"}
+        
     else {
-        mortgageTermWarning.style.display = "none"
-    }
+        mortgageTermWarning.style.display = "none"}
     
     if (interestRateInput.value === ""){
         interestRateWarning.style.display = "flex"
-    }
+        iconInterestRate.style.backgroundColor = "red"
+        iconInterestRate.style.color = "white"}
     else {
-        interestRateWarning.style.display = "none"
-    }
-    if (mortgageAmountInput.value != "" && mortgageTermInput.value != "" && interestRateInput.value != ""){
-           calculateMortgage(mortgageAmountInput.value, interestRateInput.value, mortgageTermInput.value)
-    }
-}
+        interestRateWarning.style.display = "none"}
+    if (mortgageAmountInput.value != "" && mortgageTermInput.value != "" && interestRateInput.value != ""){calculateMortgage(mortgageAmountInput.value, interestRateInput.value, mortgageTermInput.value)}}
